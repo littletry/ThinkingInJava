5f08d0ebb47d0ca514b93fb33f101a57c707da42
LittleTry <ht@yuancore.com>
2018-07-21 16:23:54 +0800

    boolTest finish

--- src/main/java/top/huangt/ops/AllOps.java
+++ src/main/java/top/huangt/ops/AllOps.java
@@ -9,7 +9,7 @@ public class AllOps {
     }
     static void f(boolean b) {}
     static void boolTest(boolean x, boolean y) {
-        // Arithemrtic operators
+        //Arithmetic operators 算术运算符
 //        x = x * y;
 //        x = x / y;
 //        x = x % y;
@@ -19,13 +19,44 @@ public class AllOps {
 //        x--;
 //        x = +y;
 //        x = -y;
-        //Relational and logical
+        //Relational and logical 关系逻辑
 //        f(x > y);
 //        f(x >= y);
 //        f(x < y);
 //        f(x <= y);
         f(x == y);
         f(x != y);
+        f(!y);
+        x = x && y;
+        x = x || y;
+        //Bitwise operators 位操作符
+//        x = ~y;
+        x = x & y;
+        x = x | y;
+        x = x ^ y;
+//        x = x << 1;
+//        x = x >> 1;
+//        x = x >>> 1;
+        //Compound assignment 复合赋值
+//        x += y;
+//        x -= y;
+//        x *= y;
+//        x /= y;
+//        x %= y;
+//        x <<= y;
+//        x >>= y;
+//        x >>>= 1;
+        x &= y;
+        x ^= y;
+        x |= y;
+        //Casting 强转
+//        char c = (char)c;
+//        byte b = (byte)x;
+//        short s = (short)x;
+//        int i = (int)x;
+//        long l = (long)x;
+//        float f = (float)x;
+//        double d = (double)x;
     }
 
 }
