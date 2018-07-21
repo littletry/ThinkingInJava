4545b700b82ec7b746d75d77f33d04c32a0470c3
LittleTry <ht@yuancore.com>
2018-07-21 16:10:10 +0800

    first commit

---
+++ src/main/java/top/huangt/ops/AllOps.java
@@ -0,0 +1,31 @@
+package top.huangt.ops;
+
+/**
+ * @author ht
+ */
+public class AllOps {
+    public static void main(String[] args){
+
+    }
+    static void f(boolean b) {}
+    static void boolTest(boolean x, boolean y) {
+        // Arithemrtic operators
+//        x = x * y;
+//        x = x / y;
+//        x = x % y;
+//        x = x + y;
+//        x = x - y;
+//        x++;
+//        x--;
+//        x = +y;
+//        x = -y;
+        //Relational and logical
+//        f(x > y);
+//        f(x >= y);
+//        f(x < y);
+//        f(x <= y);
+        f(x == y);
+        f(x != y);
+    }
+
+}
